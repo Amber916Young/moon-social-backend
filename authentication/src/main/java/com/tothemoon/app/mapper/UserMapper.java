ff975f515f0369c732989c6d03714c1b25a6aa38
birdflytomoon <yejia.yang@mycloudpa.com>
2024-02-16 17:31:07 +0000

    change some model

--- authentication/src/main/java/com/tothemoon/app/mapper/UserMapper.java
+++ authentication/src/main/java/com/tothemoon/app/mapper/UserMapper.java
@@ -1,16 +1,16 @@
 package com.tothemoon.app.mapper;
 
-import com.tothemoon.app.dto.MemberDTO;
-import com.tothemoon.common.entity.Member;
+import com.tothemoon.app.dto.UserDTO;
+import com.tothemoon.common.entity.User;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {})
 public interface UserMapper {
-    MemberDTO toDTO(Member member);
+    UserDTO toDTO(User user);
 
-    Member toEntity(MemberDTO memberDTO);
+    User toEntity(UserDTO userDTO);
 
-    List<MemberDTO> toDTOList(List<Member> members);
+    List<UserDTO> toDTOList(List<User> users);
 }
